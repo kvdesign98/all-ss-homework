860072f7a0736647a707a9ab0934eda616e216c6
vodaihuu42@gmail.com <>
2022-01-07 19:01:41 +0700

    fix deploy

---
+++ s7/index.js
@@ -0,0 +1,22 @@
+// 1.Viết chương trình tính căn bậc 2 của các số ngẫu nhiên
+//     let a = (Math.sqrt(prompt("nhap bien 1")));
+//     let b = (Math.sqrt(prompt("nhap bien 2")));
+//     console.log((a));
+//     console.log((b));
+// 2. Viết chương trình tính diện tích của một tam giác
+//     let h=prompt("chiều cao");
+//     let a=prompt("chiều dài cạch đáy")
+//     let s=(a*h)*1/2;
+//     console.log("diện tích là :",s)
+// 3.Viết chương trình chuyển từ độ F sang độ C
+//     let a=prompt("nhập độ F");
+//     let b=prompt("nhập độ F");
+//     let c=5/9*(a-32);
+//     let d=5/9*(b-32);
+//     console.log("độ C thứ nhất:",c)
+//     console.log("độ C thứ hight:",d)
+// 4.Viết chương trình chuyển từ km sang m
+//     let a=prompt("Nhập quãng đường km");
+//     let b=prompt("Nhập quãng đường km");
+//     console.log("Số m là :",a*1000);
+//     console.log("Số m là :",b*1000);
