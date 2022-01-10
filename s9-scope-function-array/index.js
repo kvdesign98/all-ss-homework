81a8b426540c1d57e00651067be3ddc660677cc7
vodaihuu42@gmail.com <>
2022-01-10 15:48:00 +0700

    fix deploy

--- s9-scope-function-array/index.js
+++ s9-scope-function-array/index.js
@@ -1,57 +1,57 @@
-// // W1
-// function tuoi(old){
-//     let old=true;
-//     if(old>18){
-//         old=true;
-//         console.log(old);
-//     }
-//     else{
-//         old=false;
-//         console.log(old);
-//     }
+// W1
+function tuoi(old){
+    let old=true;
+    if(old>18){
+        old=true;
+        console.log(old);
+    }
+    else{
+        old=false;
+        console.log(old);
+    }
 
-//     old = (old>18)?console.log(old=true):console.log(old=false)
-//     return old;
-// }
+    old = (old>18)?console.log(old=true):console.log(old=false)
+    return old;
+}
 
-// function checkAge(age) {
-//     if (age > 18) {
-//       return true;
-//     } else {
-//       return confirm('Did parents allow you?');
-//     }
-// // ?
-//     age=(age>18)?console.log(age=true):console.log('Did parents allow you?')
-// // or ||
-//     age=(age>18);alert(age=true)||alert('Did parents allow you?')
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      return confirm('Did parents allow you?');
+    }
+// ?
+    age=(age>18)?console.log(age=true):console.log('Did parents allow you?')
+// or ||
+    age=(age>18);alert(age=true)||alert('Did parents allow you?')
 
-//     return age;
-// }
+    return age;
+}
 
-// // W2
-// function min(a,b){
-//     if(a<b||b<a){
-//         if(a<b){
-//             console.log(a)
-//         }else{
-//             console.log(b)
-//         }
-//     }
-// }
+// W2
+function min(a,b){
+    if(a<b||b<a){
+        if(a<b){
+            console.log(a)
+        }else{
+            console.log(b)
+        }
+    }
+}
 
-// // W3
-// ask=(question, yes, no)=> {
-//     if (confirm(question)) yes()
-//     else no();
-//   }
+// W3
+ask=(question, yes, no)=> {
+    if (confirm(question)) yes()
+    else no();
+  }
 
-//   ask(
-//     "Do you agree?",
-//     () => { alert("You agreed."); },
-//     () => { alert("You canceled the execution."); }
-//   );
+  ask(
+    "Do you agree?",
+    () => { alert("You agreed."); },
+    () => { alert("You canceled the execution."); }
+  );
 
-// // W4
+// W4
 
 let dangNhap = prompt("Yêu Cầu Đăng Nhập")
 
