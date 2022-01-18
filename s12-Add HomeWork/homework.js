8d16618f822dcbffe99616ce459a7503bfe3318b
vodaihuu42@gmail.com <>
2022-01-18 19:15:00 +0700

    fix deploy

--- s12-Add HomeWork/homework.js
+++ s12-Add HomeWork/homework.js
@@ -3,12 +3,19 @@ const password = document.getElementById("password")
 const submit = document.getElementById("submit")
 const error = document.getElementById("error")
 const errorText = document.getElementById("error-text")
-function run() {
+const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;  
+submit.addEventListener("click", function () {
     if (email) {
         if (email.value === "") {
             alert("Error email")
         }
+        if (!filter.test(email.value)) {
+            errorText.innerHTML = "Sai kiểu dữ liệu"
+            errorText.style.color = "red"
+        }
     }
+})
+submit.addEventListener("click", function () {
     if (password) {
         if (password.value === "") {
             alert("Error password")
@@ -17,8 +24,8 @@ function run() {
             error.innerHTML = "Không đủ 8 ký tự của password hãy nhập lại"
             error.style.color = "red"
         }
-        if (password.value.length >= 8) {
+        else if (password.value.length >= 8) {
             error.innerHTML = ""
         }
     }
-}   
+})
